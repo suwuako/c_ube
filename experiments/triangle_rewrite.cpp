36498c42abdf2707e61283f7d88100b6acffa786
suwa <suwuako@protonmail.com>
2024-06-11 06:48:34 +1000

    added depth buffer

--- experiments/triangle_rewrite.cpp
+++ experiments/triangle_rewrite.cpp
@@ -10,10 +10,15 @@
 #endif
 
 
-const int triangle_offset = 10;
+const int triangle_offset = 20;
+const char lightmap[] = {'#', '-'};
 
-bool in_triangle(double* p_vertices, int x, int y);
+bool in_triangle(double* p_vertices, double* A, double* B, double* C, int x, int y);
+double get_depth_buffer(double* A, double* B, double* C, int x, int y);
 void get_screen_size(int* p_screen);
+void render_frame(double* p_triangles, int terminal_x, int terminal_y);
+void assign_coordinates(double* p_triangles, double* A, double* B, double* C, int triangle_iteration);
+char get_pixel(double* p_triangles, int x, int y);
 
 struct coordinate {
     double x;
@@ -25,29 +30,32 @@ int main()
 {
     int screen_size[2];
     get_screen_size(&screen_size[0]);
+    
+    int terminal_x = screen_size[0];
+    int terminal_y = screen_size[1];
 
-    double centre_x = screen_size[0] / 2;
-    double centre_y = screen_size[1] / 2;
-    double centre_z = (screen_size[0] + screen_size[1]) / 2;
+    double centre_x = terminal_x / 2;
+    double centre_y = terminal_y / 2;
+    double centre_z = (terminal_x + terminal_y) / 2;
 
-    double triangles[3][2] = {
-        {35, 70},
-        {20, 40},
-        {50, 20}
-    };
-        
-    double* p_triangles = &triangles[0][0];
+    double triangles[2][3][3] = {
+        {
+			{centre_x + triangle_offset, centre_y, centre_z}, 
+			{centre_x, centre_y + triangle_offset, centre_z}, 
+			{centre_x, centre_y, centre_z + triangle_offset}
+		},
+		{
+			{centre_x, centre_y, centre_z}, 
+			{centre_x + triangle_offset, centre_y, centre_z + triangle_offset},
+			{centre_x + triangle_offset, centre_y + (triangle_offset / 2), centre_z + triangle_offset}
+		}
+
+	};      
+    double* p_triangles = &triangles[0][0][0];
 
     std::cout << "dimensions: " << screen_size[0] << ", " << screen_size[1] << '\n';
-
-    for (int x = 0; x < screen_size[0]; x++)
-    {
-        for (int y = 0; y < screen_size[1]; y++)
-        {
-            std::cout << in_triangle(p_triangles, x, y);
-        }
-        std::cout << '\n';
-    }
+    render_frame(p_triangles, terminal_x, terminal_y);
+    
     return 0;
 }
 
@@ -86,14 +94,14 @@ void get_screen_size(int* p_screen)
     *screen_col = columns;
 }
 
-bool in_triangle(double* p_vertices, int x, int y)
+bool in_triangle(double* p_vertices, double* A, double* B, double* C, int x, int y)
 {
-    double A_x = *p_vertices;
-    double A_y = *(p_vertices + 1);
-    double B_x = *(p_vertices + 2);
-    double B_y = *(p_vertices + 3);
-    double C_x = *(p_vertices + 4);
-    double C_y = *(p_vertices + 5);
+    double A_x = *A;
+    double A_y = *(A + 1);
+    double B_x = *B;
+    double B_y = *(B + 1);
+    double C_x = *C;
+    double C_y = *(C + 1);
 
     double alpha = ((B_y - C_y)*(x - C_x) + (C_x - B_x)*(y - C_y)) / ((B_y - C_y)*(A_x - C_x) + (C_x - B_x)*(A_y - C_y));
     double beta = ((C_y - A_y)*(x - C_x) + (A_x - C_x)*(y - C_y)) / ((B_y - C_y)*(A_x - C_x) + (C_x - B_x)*(A_y - C_y));
@@ -104,6 +112,73 @@ bool in_triangle(double* p_vertices, int x, int y)
         return true;
     }
     return false;
+}
 
-    return false;
+void assign_coordinates(double* p_triangles, double* A, double* B, double* C, int triangle_iteration)
+{
+    for (int i = 0; i < 3; i++)
+    {
+        *(A + i) = *(p_triangles + i + 9 * triangle_iteration);
+        *(B + i) = *(p_triangles + 3 + i + 9 * triangle_iteration);
+        *(C + i) = *(p_triangles + 6 + i + 9 * triangle_iteration);
+    }
+}
+
+char get_pixel(double* p_triangles, int x, int y)
+{
+    double A[3];
+    double B[3];
+    double C[3];
+
+    double point_depth;
+    double depth_buffer = 0;
+    char output_char = ' ';
+        
+    for (int triangle = 0; triangle < 2; triangle++)
+    {
+        assign_coordinates(p_triangles, A, B, C, triangle);
+        point_depth = get_depth_buffer(A, B, C, x, y);
+
+        if (in_triangle(p_triangles, A, B, C, x, y) && point_depth > depth_buffer)
+        {
+            output_char = lightmap[triangle];
+            depth_buffer = point_depth;
+        }
+    }
+    return output_char;
+}
+
+void render_frame(double* p_triangles, int terminal_x, int terminal_y)
+{
+    for (int x = 0; x < terminal_x; x++)
+    {
+        for (int y = 0; y < terminal_y; y++)
+        {
+            std::cout << get_pixel(p_triangles, x, y);
+        }
+        std::cout << '\n';
+    }
+}
+
+double get_depth_buffer(double* A, double* B, double* C, int x, int y)
+{
+    double alpha;
+    double beta;
+
+    double A_x = *A;
+    double A_y = *(A + 1);
+    double A_z = *(A + 2);
+
+    double B_x = *B;
+    double B_y = *(B + 1);
+    double B_z = *(B + 2);
+
+    double C_x = *C;
+    double C_y = *(C + 1);
+    double C_z = *(C + 2);
+
+    alpha = ((C_x - A_x) * (y - A_y) - (C_y - A_y) * (x - A_x)) / ((B_y - A_y) * (C_x - A_x) - (B_x - A_x) * (C_y - A_y));
+    beta = (x - A_x - alpha * (B_x - A_x)) / (C_x - A_x);
+
+    return A_z + alpha * (B_z - A_z) + beta * (C_z - A_z);
 }
